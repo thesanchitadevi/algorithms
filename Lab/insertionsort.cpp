a6cc3f6785658fdca90b52776edd903bf32f00ca
Sanchita Devi <thesanchitadevi@gmail.com>
2023-02-27 01:46:35 +0600

    Add files via upload

---
+++ Lab/insertionsort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+   int n, key;
+   cout << "Enter the number of elements: ";
+   cin >> n;
+   int arr[n];    //create an array with given number of elements
+   cout << "Enter elements: " ;
+   for(int i = 0; i<n; i++) {
+      cin >> arr[i];
+   }
+   int size = sizeof(arr)/sizeof(arr[0]);
+   for(int i = 1; i<size; i++) {
+      key = arr[i];//take value
+      int j = i;
+      while(j > 0 && arr[j-1]>key) {
+         arr[j] = arr[j-1];
+         j--;
+      }
+      arr[j] = key;   //insert in right place
+   }
+   cout << "Array after Sorting: ";
+    for(int i = 0; i<size; i++)
+      cout << arr[i] << " ";
+   cout << endl;
+}
