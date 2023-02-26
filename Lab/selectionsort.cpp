aa3126b7f5bbfe075524e78162a2003b313f6fcd
Sanchita Devi <thesanchitadevi@gmail.com>
2023-02-27 01:20:15 +0600

    Update selectionsort.cpp

--- Lab/selectionsort.cpp
+++ Lab/selectionsort.cpp
@@ -4,27 +4,29 @@ using namespace std;
 int main()
 {
     
-    int i,j,n,temp,min_in;
+    int n, temp, min_in;
     cout << "Size of array: ";
     cin >> n;
     int arr[n];
     cout << "Unsorted Array : ";
-    for(int i=0;i<n;i++){
+    for(int i=0; i<n; i++){
         cin >> arr[i];
     }
     cout<<endl;
     
-    for(i=0;i<n;i++)
+    for(int i=0; i<n; i++)
     {
         min_in = i;
-        for(j=i+1;j<n;j++)
-            if (arr[j] < arr[min_in])
+        for(int j=i+1; j<n; j++){
+            if (arr[j] < arr[min_in]){
                 min_in = j;
                 swap(arr[i],arr[min_in]);
+            }
+        }
     }
     cout<<"Sorted Array : ";
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+    for(int i=0; i<n; i++){
+        cout << arr[i] <<" ";
     }
     cout<<endl;
     return(0);
